dad5760db1973887af79fc6b89f68fd9018ba9eb
leyandh <2630663461@qq.com>
2022-12-30 13:45:47 +0800

    update 1.02

---
+++ hbot/hbot/plugins/group_in_out/enter.py
@@ -0,0 +1,23 @@
+# from nonebot.typing import overrides
+# class a():
+#     # @overrides(list)
+#     def __init__(self,s:str) -> None: 
+#         self.s = s
+#     def p(self,):
+#         return self.s
+# def b(c:a):
+#     d = c.p()
+#     print(d)
+
+# b(a('sb'))
+
+# n=[1]
+import requests
+class setu:
+    def get_setu(self,down=False)->str:
+        print('url') 
+        return 'url'
+
+i=setu().get_setu
+i()
+
